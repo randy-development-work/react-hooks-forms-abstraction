2f053d6f6953bfd2b668b619c5343bcfd09de2a2
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-01-14 21:27:08 +0300

    Use one state to hold firstname and lastname in an object

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Doe",
+  });
+  
 
   function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+    setFormData({
+      ...formData,
+      firstName: event.target.value,
+    });
   }
 
   function handleLastNameChange(event) {
-    setLastName(event.target.value);
+    setFormData({
+      // initial state is an object so...
+      // spread operator copies object from current version of formData object into the new state.
+      ...formData,
+      // overwriting lastName key with new value
+      lastName: event.target.value,
+    });
   }
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={formData.firstName} />
+      <input type="text" onChange={handleLastNameChange} value={formData.lastName} />
       <button type="submit">Submit</button>
     </form>
   );
